5d847adb88340925757b1c1e56ced7029d81cf03
Jian Han <hanjian@umu.com>
2026-05-11 09:25:34 +0800

    单例

--- creational/singleton/go/singleton.go
+++ creational/singleton/go/singleton.go
@@ -2,40 +2,55 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sync"
+	"time"
 )
 
-type Config struct {
-	DSN      string
-	LogLevel string
-	Port     int
+type Logger interface {
+	Debug(string)
+	Info(string)
+}
+
+type logger struct {
+	mu sync.Mutex
+}
+
+func (l *logger) Debug(msg string) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	fmt.Fprintf(os.Stdout, "[DEBUG] %s %s\n", time.Now().Format(time.DateTime), msg)
+}
+
+func (l *logger) Info(msg string) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	fmt.Fprintf(os.Stdout, "[INFO]  %s %s\n", time.Now().Format(time.DateTime), msg)
 }
 
 var (
-	config     *Config
-	configOnce sync.Once
+	once      sync.Once
+	singleton *logger
 )
 
-func GetConfig() *Config {
-	configOnce.Do(func() {
-		config = &Config{
-			DSN:      "postgres://localhost:5432/singleton",
-			LogLevel: "info",
-			Port:     8080,
-		}
-		fmt.Println("Config loaded")
+func GetLogger() Logger {
+	once.Do(func() {
+		singleton = &logger{}
 	})
-	return config
+	return singleton
 }
 
 func main() {
 	var wg sync.WaitGroup
 
-	for range 5 {
-		wg.Go(func() {
-			cfg := GetConfig()
-			fmt.Printf("addr: %p, port: %d\n", cfg, cfg.Port)
-		})
+	for i := range 5 {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			l := GetLogger()
+			fmt.Printf("goroutine %d → addr: %p\n", n, l)
+			l.Info(fmt.Sprintf("message from goroutine %d", n))
+		}(i)
 	}
 
 	wg.Wait()
